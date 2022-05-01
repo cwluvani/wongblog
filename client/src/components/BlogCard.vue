a2bece2f97249985f9fb366b44e02fbd14f21c9e
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-01 17:03:07 +0800

    add EditBlog

--- client/src/components/BlogCard.vue
+++ client/src/components/BlogCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="blog-card">
       <div v-show="editPost" class="icons">
-          <div class="icon">
+          <div @click="editPost" class="icon">
               <svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="edit" class="edit svg-inline--fa fa-edit fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z"></path></svg>
           </div>
           <div class="icon">
@@ -14,7 +14,7 @@
       <div class="info">
           <h4> {{ cardContent.blogTitle }} </h4>
           <h6> Posted on: {{ new Date(cardContent.blogDate).toLocaleString("en-us", { dateStyle: "long" }) }} </h6>
-          <router-link class="link" to="#">
+          <router-link class="link" :to="{ name: 'ViewBlog', params: { blogid: cardContent.blogID } }">
               View The Post <svg aria-hidden="true" focusable="false" data-prefix="fal" data-icon="arrow-right" class="arrow svg-inline--fa fa-arrow-right fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M216.464 36.465l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L387.887 239H12c-6.627 0-12 5.373-12 12v10c0 6.627 5.373 12 12 12h375.887L209.393 451.494c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l211.051-211.05c4.686-4.686 4.686-12.284 0-16.971L233.434 36.465c-4.686-4.687-12.284-4.687-16.97 0z"></path></svg>
           </router-link>
       </div>
@@ -22,8 +22,9 @@
 </template>
 
 <script>
-import { computed } from 'vue';
+import { computed } from '@vue/reactivity';
 import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
 
 export default {
     name: 'BlogCard',
@@ -33,10 +34,13 @@ export default {
         cardContent: Object
     },
 
-    setup() {
+    setup(props) {
       const store = useStore();
+      const router = useRouter();
       const editPost = computed(() => store.state.editPost);
-
+      const editPost = function() {
+        router.push({ name: 'EditPost', params: { blogid: props.cardContent.blogID } })
+      };
       return {
         editPost
       }
--- client/src/pages/EditBlog.vue
+++ client/src/pages/EditBlog.vue
@@ -1,23 +1,266 @@
 <template>
-  <div></div>
+  <div class="create-post">
+    <BlogCoverPreview v-show="false" />
+    <Loading v-show="loading" />
+
+    <div class="container">
+      <div :class="{ invisible: !error }" class="err-message">
+        <p><span>Error:</span>{{ errorMsg }}</p>
+      </div>
+      <div class="blog-info">
+        <input type="text" placeholder="Enter Blog Title" v-model="blogTitle"/>
+        <div class="upload-file">
+          <label for="blog-photo">Upload Cover Photo</label>
+          <input type="file" ref="blogPhoto" id="blog-photo" @change="fileChange" accept=".png, .jpg, .jpeg" />
+          <button class="preview" :class="{ 'button-inactive': !this.$store.state.blogPhotoFileURL }">
+            Preview Photo
+          </button>
+          <span>File Chose: {{ this.$store.state.blogPhotoName }}</span>
+        </div>
+      </div>
+      <div class="editor">
+        <vue-editor v-model="blogHTML"></vue-editor>
+      </div>
+      <div class="blog-actions">
+        <button>Publish Blog</button>
+        <router-link class="router-button" :to="{ name: 'BlogPreview' }"
+          >Post Preview</router-link
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import BlogCoverPreview from "../components/BlogCoverPreview.vue";
+import Loading from "../components/Loading.vue";
+import { computed, reactive, ref } from "@vue/reactivity";
+import { useStore } from "vuex";
+// import Quill from "quill";
+import { VueEditor, Quill } from 'vue3-editor'
+import { onMounted } from '@vue/runtime-core';
+import { useRoute } from 'vue-router';
+
+window.Quill = Quill;
+const ImageResize = require("quill-image-resize-module").default;
+Quill.register("modules/imageResize", ImageResize);
+
 export default {
-    name: 'EditBlog',
-    components: {
+  name: "CreatePost",
+  components: {
+    BlogCoverPreview,
+    Loading,
+    VueEditor
+  },
 
-    },
+  setup() {
+    const store = useStore();
+    const route = useRoute();
 
-    setup() {
+    const error = ref(null);
+    const errorMsg = ref(null);
+    const loading = ref(null);
+    const file = ref(null);
+    const editorSettings = reactive({
+      modules: {
+        ImageResize: {},
+      },
+    });
 
-        return {
-            
-        }
-    }
-}
+    // const blogPhoto = ref(null);
+    // const fileChange = function () {
+    //   file.value = blogPhoto.value.files[0];
+    //   const fileName = file.value.name;
+    //   store.commit("fileNameChange", fileName);
+    //   store.commit("createFileURL", URL.createObjectURL(file.value));
+    // };
+    // const openPreview = function () {
+    //   store.commit("openPhotoPreview");
+    // };
+    // const imageHandler = function (
+    //   file,
+    //   Editor,
+    //   cursorLocation,
+    //   resetUploader
+    // ) {
+      
+    // };
+    // const openPreview = function() {
+    //   store.commit('openPhotoPreview');
+    // };
+    // const uploadBlog = function() {
+
+    // };
+
+    const profileId = computed(() => store.state.profileId);
+    const blogCoverPhotoName = computed(() => store.state.blogPhotoName);
+    const blogTitle = computed({
+      get: () => store.state.blogTitle,
+      set: payload => store.commit('updateBlogTitle', payload)
+    });
+    const blogHTML = computed({
+      get: () => store.state.blogHTML,
+      set: payload => store.commit('newBlogPost', payload)
+    });
+    
+    const routeID = ref(null);
+    const currentBlog = ref(null);
+    onMounted(async () => {
+      routeID.value = route.params.blogid;
+      currentBlog.value = await store.state.blogPosts.filter(post => post.postID === routeID.value);
+      store.commit('setBlogState', currentBlog[0]);
+    });
+
+    return {
+      error,
+      errorMsg,
+      loading,
+      file,
+      editorSettings,
+      profileId,
+      blogCoverPhotoName,
+      blogTitle,
+      blogHTML
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.create-post {
+  position: relative;
+  height: 100%;
 
+  button {
+    margin-top: 0;
+  }
+
+  .router-button {
+    text-decoration: none;
+    color: #fff;
+  }
+
+  label,
+  button,
+  .router-button {
+    transition: 0.5s ease-in-out all;
+    align-self: center;
+    font-size: 14px;
+    cursor: pointer;
+    border-radius: 20px;
+    padding: 12px 24px;
+    color: #fff;
+    background-color: #303030;
+    text-decoration: none;
+
+    &:hover {
+      background-color: rgba(48, 48, 48, 0.7);
+    }
+  }
+
+  .container {
+    position: relative;
+    height: 100%;
+    padding: 10px 25px 60px;
+  }
+
+  // error styling
+  .invisible {
+    opacity: 0 !important;
+  }
+
+  .err-message {
+    width: 100%;
+    padding: 12px;
+    border-radius: 8px;
+    color: #fff;
+    margin-bottom: 10px;
+    background-color: #303030;
+    opacity: 1;
+    transition: 0.5s ease all;
+
+    p {
+      font-size: 14px;
+    }
+
+    span {
+      font-weight: 600;
+    }
+  }
+
+  .blog-info {
+    display: flex;
+    margin-bottom: 32px;
+
+    input:nth-child(1) {
+      min-width: 300px;
+    }
+
+    input {
+      transition: 0.5s ease-in-out all;
+      padding: 10px 4px;
+      border: none;
+      border-bottom: 1px solid #303030;
+
+      &:focus {
+        outline: none;
+        box-shadow: 0 1px 0 0 #303030;
+      }
+    }
+
+    .upload-file {
+      flex: 1;
+      margin-left: 16px;
+      position: relative;
+      display: flex;
+
+      input {
+        display: none;
+      }
+
+      .preview {
+        margin-left: 16px;
+        text-transform: initial;
+      }
+
+      span {
+        font-size: 12px;
+        margin-left: 16px;
+        align-self: center;
+      }
+    }
+  }
+
+  .editor {
+    height: 60vh;
+    display: flex;
+    flex-direction: column;
+
+    .quillWrapper {
+      position: relative;
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+    }
+
+    .ql-container {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      overflow: scroll;
+    }
+
+    .ql-editor {
+      padding: 20px 16px 30px;
+    }
+  }
+
+  .blog-actions {
+    margin-top: 32px;
+
+    button {
+      margin-right: 16px;
+    }
+  }
+}
 </style>
