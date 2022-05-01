a6472c16e23dda4a717b1ccaa24583dffe77d317
Chris Wong <55499491+cwluvani@users.noreply.github.com>
2022-05-01 16:29:11 +0800

    add viewblog

--- client/src/pages/ViewBlog.vue
+++ client/src/pages/ViewBlog.vue
@@ -1,8 +1,18 @@
 <template>
-  <div></div>
+  <div class="post-view">
+    <div class="container quillWrapper">
+      <h2>{{ currentBlog[0].blogTitle }}</h2>
+      <h4>Posted on: {{ new Date(currentBlog[0].blogDate).toLocaleString("en-us", { dateStyle: "long" }) }}</h4>
+      <img :src="currentBlog[0].blogCoverPhoto" alt="">
+      <div class="post-content ql-editor" v-html="currentBlog[0].blogHTML"></div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
+import { onMounted } from '@vue/runtime-core';
+import { useRoute } from 'vue-router';
 export default {
     name: 'ViewBlog',
     components: {
@@ -10,14 +20,24 @@ export default {
     },
 
     setup() {
-
+      const currentBlog = ref(null);
+      const route = useRoute();
+      onMounted(async () => {
+        currentBlog.value = await store.state.blogPosts.filter(post => post.postID === route.params.blogid);
+      });
         return {
-            
+          currentBlog
         }
     }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.post-view {
+  h4 {
+    font-weight: 400;
+    font-size: 14px;
+    margin-bottom: 24px;
+  }
+}
 </style>
